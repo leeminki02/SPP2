1fd7368c2fbbef25820bf62b829eef34f41b71b8
kyuyoungking <152000399+kyuyoungking@users.noreply.github.com>
2024-05-31 05:11:02 +0900

    Create qr.cpp

---
+++ qr.cpp
@@ -0,0 +1,34 @@
+#include "qr.h"
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/core.hpp>
+#include <opencv2/objdetect.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int qr_detector(){
+    QRCodeDetector qrDecoder = QRCodeDetector::QRCodeDetector();
+
+    VideoCapture cap(0);
+    if (!cap.isOpened()) {
+        cerr << "Error opening video stream" << endl;
+        return -1;
+    }
+
+    Mat frame;
+    while (true) {
+        cap >> frame;
+        if (frame.empty()) {
+            cerr << "Error capturing frame" << endl;
+            break;
+        }
+
+        string data = qrDecoder.detectAndDecode(frame);
+        if (!data.empty()) {
+            cout << "QR Code detected: " << data << endl;
+            return data
+        }
+    }
+}
